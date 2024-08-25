c5e24a7b0c2bfe362858caad07d69529ef4fda6b
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-08-25 21:01:00 +0300

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -51,6 +51,22 @@ class UserProfile(models.Model):
         return f"{self.user.username} - {self.role}"
 
 
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
+    
+    def __str__(self):
+        return self.title
 
 
 
