bd0701d1c3ce57f388ad8690488229aa56df9562
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-08-25 21:50:21 +0300

    Add files via upload

---
+++ advanced_features_and_security/relationship_app/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+
+
+class RelationshipAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'relationship_app'
+from django.apps import AppConfig
+
+class RelationshipAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'relationship_app'
+
+    def ready(self):
+        import relationship_app.signals
---
+++ advanced_features_and_security/relationship_app/urls.py
@@ -0,0 +1,42 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from django.urls import path
+... from .views import list_books, LibraryDetailView
+... 
+... urlpatterns = [
+...     path('books/', list_books, name='list_books'),
+...     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+... ]
+
+"views.register"
+
+from django.urls import path
+from .views import register
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register, name='register'),
+]
+
+from django.urls import path
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns = [
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
+]
+
+
+"add_book/", "edit_book/"
+
+from django.urls import path
+from .views import add_book, edit_book, delete_book
+
+urlpatterns = [
+    path('add-book/', add_book, name='add_book'),
+    path('edit-book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete-book/<int:book_id>/', delete_book, name='delete_book'),
+]
