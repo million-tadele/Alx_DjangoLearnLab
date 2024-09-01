5564bd8271683e600914068e2a705ab6a0768562
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-01 20:09:39 +0300

    Update settings.py

--- advanced_features_and_security/relationship_app/settings.py
+++ advanced_features_and_security/relationship_app/settings.py
@@ -1,3 +1,5 @@
 Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> AUTH_USER_MODEL = 'your_app_name.CustomUser'
+# settings.py
+AUTH_USER_MODEL = 'your_app_name.CustomUser'
