1dad10a54efcc70588bd6737cb2548e9743ef0c2
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-01 20:14:20 +0300

    Update admin.py

--- advanced_features_and_security/relationship_app/admin.py
+++ advanced_features_and_security/relationship_app/admin.py
@@ -15,3 +15,16 @@ class CustomUserAdmin(UserAdmin):
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
