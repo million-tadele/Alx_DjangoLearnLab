4043ccb01648c43c39327bc9d681f22b26612526
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-01 20:53:11 +0300

    Update setting.py

--- advanced_features_and_security/LibraryProject/bookshelf/setting.py
+++ advanced_features_and_security/LibraryProject/bookshelf/setting.py
@@ -2,3 +2,10 @@ Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> # settings.py
 ... AUTH_USER_MODEL = 'your_app_name.CustomUser'
+
+
+from django.conf import settings
+
+# Example usage in a foreign key
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
