0a9d12148b94a5bf1559a63a1395ae3f608c06c8
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-01 22:03:44 +0300

    Update setting.py

--- advanced_features_and_security/LibraryProject/setting.py
+++ advanced_features_and_security/LibraryProject/setting.py
@@ -3,3 +3,14 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> # settings.py
 ... AUTH_USER_MODEL = 'your_app_name.CustomUser'
 DEBUG = False
+SECURE_BROWSER_XSS_FILTER = True  # Enable XSS protection in supported browsers
+X_FRAME_OPTIONS = 'DENY'  # Prevent clickjacking by not allowing your site to be embedded in frames
+SECURE_CONTENT_TYPE_NOSNIFF = True  # Prevent MIME type sniffing
+
+CSRF_COOKIE_SECURE = True  # Ensure CSRF cookies are sent over HTTPS only
+SESSION_COOKIE_SECURE = True  # Ensure session cookies are sent over HTTPS only
+
+SECURE_HSTS_SECONDS = 3600  # Enable HTTP Strict Transport Security for 1 hour
+SECURE_SSL_REDIRECT = True  # Redirect all non-HTTPS requests to HTTPS
+
+
