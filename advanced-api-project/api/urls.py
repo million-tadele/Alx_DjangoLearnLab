901c515358d349005da47f7dd10c541f6499be64
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-08 21:10:31 +0300

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,7 @@
-Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 >>> # api/urls.py
+
+"books/update", "books/delete"
 ... from django.urls import path
 ... from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 ... 
