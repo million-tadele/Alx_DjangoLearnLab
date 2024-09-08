8b4c15d511afeee5b36783067b54102c9fae51b7
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-08 21:02:18 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
 # Create your views here.
+# api/views.py
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListView(generics.ListAPIView):
+    """
+    Retrieves a list of all books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    Retrieves a single book by ID.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
+
+
+class BookCreateView(generics.CreateAPIView):
+    """
+    Creates a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    Updates an existing book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    """
+    Deletes an existing book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
+
