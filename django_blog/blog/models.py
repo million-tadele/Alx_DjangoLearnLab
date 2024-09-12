29057cfa39f1db6af5e373654a67e215de7beacd
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-12 10:44:18 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -31,3 +31,16 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+from django.db import models
+from django.contrib.auth.models import User
+from .models import Post
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
