81a6f18ff65440afd0130fa2ea53918a5d9d1910
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-12 11:32:06 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -28,3 +28,4 @@ urlpatterns = [
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
 "comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"
+"tags/<slug:tag_slug>/", "PostByTagListView.as_view()"
