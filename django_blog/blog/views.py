7feacbd1eddc6f133b40bc9386039c4e2f2dd378
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-12 09:53:00 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout
+from .forms import UserRegisterForm
+from django.contrib import messages
+
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Profile view
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
