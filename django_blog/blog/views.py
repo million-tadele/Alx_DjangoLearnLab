0fa2ccc50756ee846b350bf0320e3a12a95029a3
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-12 11:21:28 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -119,3 +119,15 @@ class CommentDeleteView(UserPassesTestMixin, DeleteView):
         comment = self.get_object()
         return self.request.user == comment.author
 "CommentCreateView"
+from django.db.models import Q
+from django.shortcuts import render
+from .models import Post
+
+def search_posts(request):
+    query = request.GET.get('q')
+    results = []
+    if query:
+        results = Post.objects.filter(
+            Q(title__icontains=query) | Q(content__icontains=query) | Q(tags__name__icontains=query)
+        ).distinct()
+    return render(request, 'blog/search_results.html', {'results': results, 'query': query})
