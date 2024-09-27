9020ed789ab63b7f42821a856aef8485f9e90b3f
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-27 13:10:20 +0300

    Add files via upload

---
+++ social_media_api/notifications/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Notification
+from .serializers import NotificationSerializer
+
+class NotificationListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        notifications = Notification.objects.filter(recipient=request.user).order_by('-timestamp')
+        serializer = NotificationSerializer(notifications, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        Notification.objects.filter(recipient=request.user, read=False).update(read=True)
+        return Response({'status': 'Notifications marked as read'})
