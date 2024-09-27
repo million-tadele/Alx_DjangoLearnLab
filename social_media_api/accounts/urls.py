4de4c5bd3483732a551f7bcc70b65a989ff6e37b
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-27 07:11:26 +0300

    Add files via upload

---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import RegisterView, UserProfileView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+]
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', obtain_auth_token, name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+]
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics, permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = response.data
+        token, created = Token.objects.get_or_create(user=self.request.user)
+        return Response({'token': token.key, 'user': user})
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
