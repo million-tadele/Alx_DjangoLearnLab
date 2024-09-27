bf581665ff392a079ea9378ab545c67306f052c4
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-27 11:42:04 +0300

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -25,3 +25,16 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+from django.conf import settings
+from django.db import models
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Prevents a user from liking the same post multiple times
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.title}"
