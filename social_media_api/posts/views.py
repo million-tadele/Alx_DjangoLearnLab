9b2809393ac8e6f6fbe9d8ba7a1b0d2bb0a49826
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-09-27 11:14:01 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -29,3 +29,16 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['title', 'content']
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        followed_users = user.following.all()
+        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
